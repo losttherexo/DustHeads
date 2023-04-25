0ff2be162895253ff031c03a87758424ba062a49
Andre Vargas Roo <a.vargas252@icloud.com>
2023-04-25 12:30:34 -0400

    working through backend

--- server/models.py
+++ server/models.py
@@ -1,62 +1,103 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.hybrid import hybrid_property
+from datetime import datetime
 from config import db
 
-class DustHead(db.Model, SerializerMixin):
-    __tablename__ = 'dustheads'
-
+class DustHead(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(16), nullable = False)
-    first_name = db.Column(db.String, nullable = False)
-
-
-    copies = db.relationship('Copy', backref='dusthead', lazy=True)
-    follows = db.relationship('Follow', foreign_keys='Follow.follower_id', backref='follower', lazy='dynamic', cascade='all, delete-orphan')
-    followed_by = db.relationship('Follow', foreign_keys='Follow.followed_id', backref='followed', lazy='dynamic', cascade='all, delete-orphan')
+    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    collections = db.relationship('Copy', backref='dusthead', lazy=True)
+    followed = db.relationship('Follow',
+                                foreign_keys='Follow.follower_id',
+                                backref='follower', lazy='dynamic')
+    followers = db.relationship('Follow',
+                                foreign_keys='Follow.followed_id',
+                                backref='followed', lazy='dynamic')
     comments = db.relationship('Comment', backref='dusthead', lazy=True)
+    likes = db.relationship('Like', backref='dusthead', lazy=True)
 
-class Artist(db.Model, SerializerMixin):
-    __tablename__ = 'artists'
+    def follow(self, dusthead):
+        if not self.is_following(dusthead):
+            f = Follow(follower=self, followed=dusthead)
+            db.session.add(f)
+            db.session.commit()
 
+    def unfollow(self, dusthead):
+        f = self.followed.filter_by(followed_id=dusthead.id).first()
+        if f:
+            db.session.delete(f)
+            db.session.commit()
+
+    def is_following(self, dusthead):
+        return self.followed.filter_by(followed_id=dusthead.id).first() is not None
+
+    def followed_collections(self):
+        followed = Copy.query.join(Follow, (Follow.followed_id == Copy.dusthead_id)).filter(Follow.follower_id == self.id)
+        own = Copy.query.filter_by(dusthead_id=self.id)
+        return followed.union(own).order_by(Copy.timestamp.desc())
+
+    def __repr__(self):
+        return '<DustHead {}>'.format(self.username)
+
+
+class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
-    records = db.relationship('Record', backref='artist', lazy=True)
-
-class Record(db.Model, SerializerMixin):
-    __tablename__ = 'records'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255))
+    title = db.Column(db.String(255), nullable=False)
+    genre = db.Column(db.String(64), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
     copies = db.relationship('Copy', backref='record', lazy=True)
 
-class Copy(db.Model, SerializerMixin):
-    __tablename__ = 'copies'
+    def __repr__(self):
+        return '<Record {}>'.format(self.title)
 
+
+class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    likes = db.relationship('Like', backref='copy', lazy=True)
-    dusthead_id = db.Column(db.Integer, db.ForeignKey('dusthead.id'), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
+    records = db.relationship('Record', backref='artist', lazy=True)
+
+    def __repr__(self):
+        return '<Artist {}>'.format(self.name)
+
+
+class Copy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     record_id = db.Column(db.Integer, db.ForeignKey('record.id'), nullable=False)
+    dusthead_id = db.Column(db.Integer, db.ForeignKey('dust_head.id'), nullable=False)
+    condition = db.Column(db.String(255))
+    liked_by = db.relationship('Like', backref='copy', lazy=True)
+    comments = db.relationship('Comment', backref='copy', lazy=True)
+
+    def __repr__(self):
+        return '<Copy {}>'.format(self.id)
+
+
+class Follow(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    follower_id = db.Column(db.Integer, db.ForeignKey('dust_head.id'))
+    followed_id = db.Column(db.Integer, db.ForeignKey('dust_head.id'))
+
+    def __repr__(self):
+        return '<Follow {}>'.format(self.id)
     
-
-class Follow(db.Model, SerializerMixin):
-    __tablename__ = 'follows'
-
+class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    follower_id = db.Column(db.Integer, db.ForeignKey('dusthead.id'), nullable=False)
-    followed_id = db.Column(db.Integer, db.ForeignKey('dusthead.id'), nullable=False)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    dusthead_id = db.Column(db.Integer, db.ForeignKey('dust_head.id'))
+    copy_id = db.Column(db.Integer, db.ForeignKey('copy.id'))
 
-class Comment(db.Model, SerializerMixin):
-    __tablename__ = 'comments'
+    def __repr__(self):
+        return '<Comment {}>'.format(self.body)
 
+
+class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(255))
-    dusthead_id = db.Column(db.Integer, db.ForeignKey('dusthead.id'), nullable=False)
-    copy_id = db.Column(db.Integer, db.ForeignKey('copy.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    dusthead_id = db.Column(db.Integer, db.ForeignKey('dust_head.id'))
+    copy_id = db.Column(db.Integer, db.ForeignKey('copy.id'))
 
-class Like(db.Model, SerializerMixin):
-    __tablename__ = 'likes'
-
-    id = db.Column(db.Integer, primary_key=True)
-    dusthead_id = db.Column(db.Integer, db.ForeignKey('dusthead.id'), nullable=False)
-    copy_id = db.Column(db.Integer, db.ForeignKey('copy.id'), nullable=False)
+    def __repr__(self):
+        return '<Like {}>'.format(self.id)
