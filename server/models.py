03c97d076c7ba9a12b4bb6226bc8e4a6b4cde7c7
Andre Vargas Roo <a.vargas252@icloud.com>
2023-04-25 15:55:26 -0400

    tables created

--- server/models.py
+++ server/models.py
@@ -8,10 +8,12 @@ class DustHead(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    first_name = db.Column(db.String(32), nullable=False)
+    last_name = db.Column(db.String(32))
+    bio = db.Column(db.String(280))
 
+    copies = db.relationship('Copy', backref='dusthead', lazy=True)
     comments = db.relationship('Comment', backref='dusthead', lazy=True)
-    likes = db.relationship('Like', backref='dusthead', lazy=True)
 
 class Record(db.Model, SerializerMixin):
     __tablename__ = 'records'
@@ -19,10 +21,11 @@ class Record(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     genre = db.Column(db.String(64), nullable=False)
-    image = db.Column(db.String)
+    artist = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String, nullable=False)
 
     copies = db.relationship('Copy', backref='record', lazy=True)
-
 
 
 class Copy(db.Model, SerializerMixin):
@@ -30,9 +33,11 @@ class Copy(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
 
+    description = db.Column(db.String(255))
+    image = db.Column(db.String)
+
     record_id = db.Column(db.Integer, db.ForeignKey('records.id'), nullable=False)
     dusthead_id = db.Column(db.Integer, db.ForeignKey('dustheads.id'), nullable=False)
-    image = db.Column(db.String)
 
     comments = db.relationship('Comment', backref='copy', lazy=True)
 
@@ -42,7 +47,7 @@ class Comment(db.Model, SerializerMixin):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140), nullable=False)
+    body = db.Column(db.String(280), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     dusthead_id = db.Column(db.Integer, db.ForeignKey('dustheads.id'), nullable=False)
