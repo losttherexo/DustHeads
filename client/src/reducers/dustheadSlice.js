0dfe64f1d9d64f058808530c165b20ff7500cfbe
Andre Vargas Roo <a.vargas252@icloud.com>
2023-04-26 19:08:28 -0400

    added dustheads to redux

--- client/src/reducers/dustheadSlice.js
+++ client/src/reducers/dustheadSlice.js
@@ -1,10 +1,30 @@
 const initialState = []
 
-export const dustheadReducer = (state = initialState, action) => {
+export const addDustHead = newDustHeadObj => {
+    return {type:'dustheads/add', payload:newDustHeadObj}
+}
 
-    switch(action.type){
-        default:
-            return state
+export const fetchRecords = () => {
+    return function(dispatch){
+        fetch('http://localhost:5555/dustheads')
+        .then(r => r.json())
+        .then(dhArray => {
+            dispatch({type:'dustheads/set', payload: dhArray})
+        })
     }
+}
 
+export const dustheadReducer = (state = initialState, action) => {
+    switch(action.type){
+        case 'dustheads/set':
+            /* assumes action.payload is an array of objects */
+            return action.payload
+        case 'dustheads/add':
+            /* assumes action.payload is an object */
+            return [...state, action.payload]
+        case 'dustheads/remove':
+            /* assumes action.payload is an integer of a doomed ID */
+            return state.filter(dhObj => dhObj.id !== action.payload)
+        default: return state
+    }
 }
