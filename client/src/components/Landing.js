f2e17e9040241c1d440a9851c8d2460bbc6837d1
Andre Vargas Roo <a.vargas252@icloud.com>
2023-04-26 15:04:20 -0400

    landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -3,29 +3,27 @@ import Vinyl from '../img/Vinyl.png'
 
 function Landing() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className='grid grid-cols-3 gap-8'>
-        <div className='flex justify-center items-center'>
-          <img src={Dusty} alt='Dusty' className='bg-transparent' />
+    <div className='flex flex-col md:flex-row h-screen items-center'>
+        <div className=''>
+          <img src={Dusty} alt='Dusty'/>
         </div>
-        <div className='col-span-1 text-center my-auto'>
+        <div className='text-center'>
           <h1 className='text-6xl font-bold mb-8'>
             DUSTHEADS
           </h1>
-          <form>
+          <form className='flex flex-col items-center'>
             <label htmlFor='username' className='block mb-2 font-medium'>Username</label>
-            <input type='text' id='username' className='w-full p-2 border border-gray-400 rounded-md mb-4' />
+            <input type='text' id='username' className='w-2/3 p-2 border border-gray-400 rounded-md mb-4' />
             <label htmlFor='password' className='block mb-2 font-medium'>Password</label>
-            <input type='password' id='password' className='w-full p-2 border border-gray-400 rounded-md mb-4' />
-            <button type='submit' className='px-4 py-2 text-white bg-gray-900 rounded-md hover:bg-gray-800'>
+            <input type='password' id='password' className='w-2/3 p-2 border border-gray-400 rounded-md mb-4' />
+            <button type='submit' className='w-2/3 px-4 py-2 text-white bg-gray-900 rounded-md hover:bg-gray-800'>
               Log In
             </button>
           </form>
         </div>
-        <div className='flex justify-center items-center'>
-          <img src={Vinyl} alt='Vinyl' />
+        <div className=''>
+          <img src={Vinyl} alt='Vinyl'/>
         </div>
-      </div>
     </div>
   );
 }
