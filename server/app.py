02a7e72a71cfa97e20dd921fba125db171ff2eb0
Andre Vargas Roo <a.vargas252@icloud.com>
2023-04-28 09:58:13 -0400

    added userSlice

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask import request, make_response, session, abort
 from flask_restful import Resource
 
 # Local imports
-from config import app, db, api, bcrypt
+from config import app, db, api
 from models import DustHead, Record, Copy, Comment
 
 
