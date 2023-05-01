d843300447cf27b6b6fad001294d46def260222c
Andre Vargas Roo <a.vargas252@icloud.com>
2023-05-01 14:12:51 -0400

    starting copy cards

--- client/src/components/DustHead.js
+++ client/src/components/DustHead.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import NavBar from "./NavBar"
-import RecordCard from "./RecordCard"
+import CopyCard from "./CopyCard"
 import { fetchCopies } from "../reducers/copySlice"
 import Recommend from "./Recommend"
 
@@ -18,8 +18,10 @@ function DustHead({id}) {
     const filteredCopies = copies.filter((c) => c.dusthead_id === id);
 
     const copyCards = filteredCopies.map((c) => (
-      <RecordCard key={c.id} title={c.record.title} image={c.record.image}/>
+      <CopyCard key={c.id} title={c.record.title} image={c.record.image}/>
     ));
+
+    console.log(dusthead.bio)
 
     return(
         <div className='flex text-gray-300'>
@@ -27,11 +29,13 @@ function DustHead({id}) {
                 <NavBar/>
             </div>
             <div className='flex-col justify-center mx-6 basis-[65%]'>
-                <div>
-                    {dusthead.username}
+                <div className='flex flex-col mx-2 mt-12'>
+                    <span className='text-6xl'>{dusthead.username}</span>
+                    <span className='my-2'>
+                        {dusthead.bio? dusthead.bio : 'welp here goes nothing'}
+                    </span>
                 </div>
                 <div>
-                    crate
                     {copyCards}
                 </div>
             </div>
