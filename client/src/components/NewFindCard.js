24e6e3a610a331c1d09d5b0fc6ea076a435200d8
Andre Vargas Roo <a.vargas252@icloud.com>
2023-05-02 16:46:42 -0400

    starting edit button for descriptions

--- client/src/components/NewFindCard.js
+++ client/src/components/NewFindCard.js
@@ -1,17 +1,30 @@
-function NewFindCard({record, dusthead, description}){
-    // console.log(record)
+import { useSelector } from 'react-redux'
 
-    return (
-        <div className='border flex flex-row my-4 h-44 text-gray-300 rounded-md justify-between'>
-            <div>
-                <p className='mx-3 mt-2'>@{dusthead.username}</p>
+function NewFindCard({ record, dusthead, dusthead_id, description }) {
+  const user = useSelector((state) => state.user)
+
+  const handleEdit = () => {
+    console.log("let's get some work done")
+  }
+
+  return (
+    <div className='border flex flex-row my-4 h-44 text-gray-300 rounded-md justify-between'>
+        <div>
+            <div className='h-[70%]'>
+                <p className='mx-3 pt-2'>@{dusthead.username}</p>
                 <p className='mx-3 mt-1'>{description}</p>
             </div>
-            <div>
-                <img src={record.image} alt={record.title} className='flex w-44 justify-center p-3' />
+            <div className='h-[30%] flex items-center'>
+                {user && dusthead_id === user.id && (
+                    <button onClick={handleEdit} className='flex justify-center mx-3 px-3 border rounded-lg hover:bg-black'>Edit</button>
+                    )}
             </div>
         </div>
-    )
+        <div>
+            <img src={record.image} alt={record.title} className='flex w-44 justify-center p-3' />
+        </div>
+    </div>
+  )
 }
 
 export default NewFindCard
