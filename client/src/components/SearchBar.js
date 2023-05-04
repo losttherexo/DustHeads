d6129a65618e2552450dd86931e6bc724a87da3d
Andre Vargas Roo <a.vargas252@icloud.com>
2023-05-04 11:16:36 -0400

    searchbar is searching

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,20 +1,48 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 function SearchBar() {
-  const records = useSelector(s=>s.records)
-  const dustheads = useSelector(s=>s.dustheads)
+  const records = useSelector(s => s.records)
+  const dustheads = useSelector(s => s.dustheads)
+  const [searchQuery, setSearchQuery] = useState('')
 
   function handleChange(e) {
+    setSearchQuery(e.target.value.toLowerCase())
+  }
+
+  let resultCards = null
+
+  if (searchQuery.trim() !== '') {
+    const filteredRecords = records.filter(
+      (record) =>
+        record.title.toLowerCase().includes(searchQuery) ||
+        record.artist.toLowerCase().includes(searchQuery)
+    )
+    const filteredDustheads = dustheads.filter((dusthead) =>
+      dusthead.username.toLowerCase().includes(searchQuery)
+    )
+
+    resultCards = [
+      ...filteredRecords.map((record) => (
+        console.log(record)
+      )),
+      ...filteredDustheads.map((dusthead) => (
+        console.log(dusthead)
+      )),
+    ]
   }
 
   return (
-    <div className='hidden md:flex border-l border-b py-4 justify-center'>
-      <input
-        type='text'
-        placeholder='Search'
-        onChange={handleChange}
-        className='mx-2 rounded-lg'
-      />
+    <div>
+      <div className='hidden md:flex border-l border-b py-4 justify-center'>
+        <input
+          type='text'
+          placeholder='Search'
+          onChange={handleChange}
+          className='mx-2 rounded-lg'
+        />
+      </div>
+      <div className='grid grid-cols-3'>{resultCards}</div>
     </div>
   );
 }
