5f90126eebc7dad7d3a85f4779479801e6e8fd88
Andre Vargas Roo <a.vargas252@icloud.com>
2023-05-10 09:52:10 -0400

    css cleanup on records page

--- client/src/components/Records.js
+++ client/src/components/Records.js
@@ -8,8 +8,6 @@ function Records() {
     
     const records = useSelector(s => s.records)
 
-    console.log(records)
-
     records.sort((a, b) => b.copies.length - a.copies.length)
 
     const recordCards = records.map(r => <RecordCard key={r.id} {...r}/>)
@@ -19,9 +17,9 @@ function Records() {
             <div className='flex-col basis-[20%]'>
                 <NavBar/>
             </div>
-            <div className='flex-col justify-center mx-6 basis-[55%]'>
-                <div className='h-36 border-b'>
-                    <h1 className='py-12 text-6xl font-bold text-center text-gray-300'>What's Poppin?</h1>
+            <div className='flex-col justify-center mx-6 basis-[55%] text-gray-300'>
+                <div className='flex flex-col h-36 border-b'>
+                    <h1 className='text-6xl flex justify-center my-auto'>Popular Records</h1>
                 </div>
                 <div className='py-6'>
                     {recordCards}   
